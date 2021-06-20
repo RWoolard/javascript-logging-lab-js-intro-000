4ec63ca940f05dcabd9382d469cd4da0c235f0f8
Rayshawn Woolard <r.woolard456@gmail.com>
2021-06-20 23:52:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ console.error("Your test failed!")
 
 console.log("You've passed 2, now try the third!")
 
-console.warn("Don't forget to save your code before running your tests!")
+console.warn("Don't forget to save your code before running")
